3da2401c24dee9a06fcc13f42405ce8cff7327b9
cesarwizeline <cesar.romero@wizeline.com>
2021-12-17 14:09:35 -0600

    checkout

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,5 +1,10 @@
 import React, { useContext } from 'react';
-import { Form, CheckoutPageContent } from './CheckoutPage.styles';
+import {
+  Form,
+  CheckoutPageContent,
+  CheckoutActions,
+  ProductsSIde,
+} from './CheckoutPage.styles';
 import { Button } from '../../utils/main.styles';
 import { useNavigate } from 'react-router-dom';
 import CartContext from '../../context/CartContext';
@@ -8,6 +13,7 @@ import {
   ProductsRow,
   RowElement,
 } from '../ShoppingCartPage/ShoppingCartPage.styles';
+import { Title } from '../../utils/main.styles';
 import InputText from '../../components/InputText';
 const CheckoutPage = () => {
   const navigate = useNavigate();
@@ -15,43 +21,46 @@ const CheckoutPage = () => {
 
   return (
     <CheckoutPageContent>
-      <Form>
-        <InputText title="Name" />
-        <InputText title="Email" />
-        <InputText title="post/zip" />
-        <InputText type="textarea" title="Order Notes" />
-      </Form>
-      <TableProducts>
-        <ProductsRow>
-          <RowElement>&nbsp;</RowElement>
-          <RowElement>&nbsp;</RowElement>
-          <RowElement>Name</RowElement>
-          <RowElement>Quantity</RowElement>
-          <RowElement>Price</RowElement>
-        </ProductsRow>
+      <ProductsSIde>
+        <Title> Checkout </Title>
+        <Form>
+          <InputText title="Name" />
+          <InputText title="Email" />
+          <InputText title="post/zip" />
+          <InputText type="textarea" title="Order Notes" />
+        </Form>
+        <TableProducts>
+          <ProductsRow>
+            <RowElement>&nbsp;</RowElement>
+            <RowElement>&nbsp;</RowElement>
+            <RowElement>Name</RowElement>
+            <RowElement>Quantity</RowElement>
+            <RowElement>Price</RowElement>
+          </ProductsRow>
 
-        {cart.products.map((product) => {
-          return (
-            <ProductsRow key={product.id}>
-              <RowElement></RowElement>
-              <RowElement>
-                <img
-                  style={{ width: '120px' }}
-                  src={product.image}
-                  alt={product.name}
-                />
-              </RowElement>
-              <RowElement>{product.name} </RowElement>
-              <RowElement>{product.quantity}</RowElement>
-              <RowElement> $ {` ${product.price.toFixed(2)}`} </RowElement>
-            </ProductsRow>
-          );
-        })}
-      </TableProducts>
-      <div>
-        <Button className={'success'}> Place order </Button>
-        <Button onClick={() => navigate('/cart')}> Go back to cart </Button>
-      </div>
+          {cart.products.map((product) => {
+            return (
+              <ProductsRow key={product.id}>
+                <RowElement></RowElement>
+                <RowElement>
+                  <img
+                    style={{ width: '120px' }}
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </RowElement>
+                <RowElement>{product.name} </RowElement>
+                <RowElement>{product.quantity}</RowElement>
+                <RowElement> $ {` ${product.price.toFixed(2)}`} </RowElement>
+              </ProductsRow>
+            );
+          })}
+        </TableProducts>
+        <CheckoutActions>
+          <Button className={'success'}> Place order </Button>
+          <Button onClick={() => navigate('/cart')}> Go back to cart </Button>
+        </CheckoutActions>
+      </ProductsSIde>
     </CheckoutPageContent>
   );
 };
